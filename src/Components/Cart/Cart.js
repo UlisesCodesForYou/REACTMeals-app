e0a6e3eb1ef32eab378a70898cca8181ab6ba435
UlisesCodesForYou <76757797+UlisesCodesForYou@users.noreply.github.com>
2022-02-17 21:40:56 -0800

    enabled data to be sent to a database.

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -26,6 +26,16 @@ const Cart = (props) => {
     setIsCheckOut(true);
   };
 
+  const submitOrderHandler = (userData) => {
+    fetch("https://menu-5b54e-default-rtdb.firebaseio.com/orders.json", {
+      method: "POST",
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items,
+      }),
+    });
+  };
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
@@ -61,7 +71,9 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckOut && <Checkout onCancel ={props.onClose} />} 
+      {isCheckOut && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
       {!isCheckOut && modalActions}
     </Modal> // in hasItems above, I have made it dynamic.
   );
--- src/Components/Cart/Checkout.js
+++ src/Components/Cart/Checkout.js
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 import classes from "./Checkout.module.css";
 
@@ -6,50 +6,100 @@ const isEmpty = (value) => value.trim() === ""; // Helper functions that are use
 const isFiveNumbs = (value) => value.trim().length === 5;
 
 const Checkout = (props) => {
+  const [formInputValidity, setFormInputValidity] = useState({
+    name: true,
+    street: true,
+    city: true,
+    postalCode: true, // I've created an object to have multiple "slices" of state.  This will allow me to check for multiple things in one shot.
+  });
+
   const nameInputRef = useRef();
   const streetInputRef = useRef();
   const postalCodeInputRef = useRef();
   const cityInputRef = useRef();
 
-  const enteredName = nameInputRef.current.value;
-  const enteredStreet = streetInputRef.current.value;
-  const enteredPostalCode = postalCodeInputRef.current.value;
-  const enteredCity = cityInputRef.current.value;
-
   const confirmHandler = (event) => {
     event.preventDefault();
 
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostalCode = postalCodeInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
+
     const nameIsValid = !isEmpty(enteredName); // So here, I am validating if the isEmpty function is NOT empty.
     const streetIsValid = !isEmpty(enteredStreet);
-    const postalIsValid = isFiveNumbs(enteredPostalCode);
+    const postalIsValid = isFiveNumbs(enteredPostalCode); // These variables now hold true or false values.
     const cityIsValid = !isEmpty(enteredCity);
 
-    const formIsValid =
-      nameIsValid && streetIsValid && postalIsValid && cityIsValid;
+    setFormInputValidity({
+      // This state has been set to give the user some feedback.  I've declared it above.
+      name: nameIsValid,
+      street: streetIsValid,
+      city: cityIsValid,
+      postalCode: postalIsValid,
+    });
 
-    if (formIsValid) {
+    const formIsValid =
+      nameIsValid && streetIsValid && postalIsValid && cityIsValid; // This form checks if all the valid variables are valid.
+
+    if (!formIsValid) {
       // Data goes here.
+      return;
     }
+    props.onConfirm({
+      name: enteredName,
+      street: enteredStreet,
+      city: enteredCity,
+      postalCode: enteredPostalCode,
+    });
   };
+
+  const nameControlClasses = `${classes.control} ${
+    formInputValidity.name ? "" : classes.invalid
+  }`;
+
+  const streetControlClasses = `${classes.control} ${
+    formInputValidity.street ? "" : classes.invalid
+  }`;
+
+  const cityControlClasses = `${classes.control} ${
+    formInputValidity.city ? "" : classes.invalid
+  }`;
+
+  const postalCodeControlClasses = `${classes.control} ${
+    formInputValidity.postalCode ? "" : classes.invalid
+  }`;
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
-      <div className={classes.control}>
+      <div className={nameControlClasses}>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" ref={nameInputRef} />
+        {!formInputValidity.name && <p>Please enter a valid name.</p>}
       </div>
-      <div className={classes.control}>
+
+      <div className={streetControlClasses}>
         <label htmlFor="street">Street</label>
         <input type="text" id="street" ref={streetInputRef} />
+        {!formInputValidity.street && (
+          <p>Please enter a valid street address.</p>
+        )}
       </div>
-      <div className={classes.control}>
+
+      <div className={postalCodeControlClasses}>
         <label htmlFor="postal">Postal Code</label>
         <input type="text" id="postal" ref={postalCodeInputRef} />
+        {!formInputValidity.postalCode && (
+          <p>Please enter a valid postal zip code with (5 characters long.).</p>
+        )}
       </div>
-      <div className={classes.control}>
+
+      <div className={cityControlClasses}>
         <label htmlFor="city">City</label>
         <input type="text" id="city" ref={cityInputRef} />
+        {!formInputValidity.city && <p>Please enter a valid city name.</p>}
       </div>
+
       <div className={classes.actions}>
         <button type="button" onClick={props.onCancel}>
           Cancel
